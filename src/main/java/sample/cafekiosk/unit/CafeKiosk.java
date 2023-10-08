9a11ff58cba3969c8da53adf6317ac363ab9a0c6
soochan <tncksdl05@gmail.com>
2023-10-08 22:43:33 +0900

    feat: 수동 테스트 및 요구 사항 기능 구현

---
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -0,0 +1,36 @@
+package sample.cafekiosk.unit;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import sample.cafekiosk.unit.beverage.Beverage;
+import sample.cafekiosk.unit.order.Order;
+
+public class CafeKiosk {
+
+    private final List<Beverage> beverages = new ArrayList<>();
+
+    public void add(Beverage beverage) {
+        beverages.add(beverage);
+    }
+
+    public void remove(Beverage beverage) {
+        beverages.remove(beverage);
+    }
+
+    public void clear() {
+        beverages.clear();
+    }
+
+    public int calculateTotalPrice() {
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
+    }
+
+    public Order createOrder() {
+        return new Order(LocalDateTime.now(), beverages);
+    }
+
+}
