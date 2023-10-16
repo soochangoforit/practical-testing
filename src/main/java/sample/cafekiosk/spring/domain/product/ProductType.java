e30708bda492831228c172a4374d9d329c2892c7
soochan <tncksdl05@gmail.com>
2023-10-16 16:12:32 +0900

    feat: 재고와 관련된 타입인지 체크하는 로직 구현

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductType.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductType.java
@@ -1,5 +1,7 @@
 package sample.cafekiosk.spring.domain.product;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -12,5 +14,12 @@ public enum ProductType {
     BAKERY("베이커리");
 
     private final String text;
+
+    /**
+     * 재고와 관련 있는 상품 타입은 병 음료, 베이커리이다
+     */
+    public static boolean containsStockType(ProductType type) {
+        return List.of(BOTTLE, BAKERY).contains(type);
+    }
 
 }
