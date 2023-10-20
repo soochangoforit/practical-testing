b1dfeaa9fabe4e266e85e7b67fedfe1bf92c862e
soochan <tncksdl05@gmail.com>
2023-10-20 23:03:13 +0900

    test: mocking을 통해 메일 전송 상태에 따른 행위 검증

---
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,83 @@
+package sample.cafekiosk.spring.api.service.mail;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import sample.cafekiosk.spring.client.mail.MailSendClient;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class MailServiceTest {
+
+    @Autowired
+    private MailService mailService;
+
+    @Autowired
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @MockBean
+    private MailSendClient mailSendClient;
+
+    @AfterEach
+    void tearDown() {
+        mailSendHistoryRepository.deleteAllInBatch();
+    }
+
+    @Test
+    @DisplayName("메일 전송이 성공하면 이력을 저장한다")
+    void sendMail() {
+        // given
+        String fromEmail = "수찬이@gmail.com";
+        String toEmail = "우테코@gmail.com";
+        String subject = "이수찬 우테코 합격 메일 전송";
+        String content = "축하합니다. 이수찬님은 우테코에 합격하셨습니다.";
+
+        // stubbing
+        when(mailSendClient.sendEmail(any(String.class), any(), any(), any()))
+                .thenReturn(true);
+
+        // when
+        boolean result = mailService.sendMail(fromEmail, toEmail, subject, content);
+
+        // then
+        assertThat(result).isTrue();
+        assertThat(mailSendHistoryRepository.findAll()).hasSize(1)
+                .extracting("fromEmail", "toEmail", "subject", "content")
+                .containsExactly(
+                        tuple(fromEmail, toEmail, subject, content)
+                );
+    }
+
+    @Test
+    @DisplayName("메일 전송이 실패하면 이력을 저장하지 않는다")
+    void sendMail_fail() {
+        // given
+        String fromEmail = "수찬이@gmail.com";
+        String toEmail = "우테코@gmail.com";
+        String subject = "이수찬 우테코 합격 메일 전송";
+        String content = "축하합니다. 이수찬님은 우테코에 합격하셨습니다.";
+
+        // stubbing
+        when(mailSendClient.sendEmail(any(String.class), any(), any(), any()))
+                .thenReturn(false);
+
+        // when
+        boolean result = mailService.sendMail(fromEmail, toEmail, subject, content);
+
+        // then
+        assertThat(result).isFalse();
+        assertThat(mailSendHistoryRepository.findAll()).isEmpty();
+    }
+
+}
