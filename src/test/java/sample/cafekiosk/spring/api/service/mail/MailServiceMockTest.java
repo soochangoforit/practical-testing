eaeb4f118ebf490825f8c3f330c4ed314d70896d
soochan <tncksdl05@gmail.com>
2023-11-30 18:13:44 +0900

    test(MailSendClient): BDD Test Style을 위해서 BDDMockito를 활용

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceMockTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceMockTest.java
@@ -3,7 +3,6 @@ package sample.cafekiosk.spring.api.service.mail;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -11,9 +10,9 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
@@ -22,7 +21,7 @@ import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 @ExtendWith(MockitoExtension.class)
 class MailServiceMockTest {
 
-    @Spy
+    @Mock
     private MailSendClient mailSendClient;
 
     @Mock
@@ -34,9 +33,9 @@ class MailServiceMockTest {
     @DisplayName("메일 전송 테스트")
     @Test
     void sendMail() {
-        doReturn(true)
-                .when(mailSendClient)
-                .sendEmail(anyString(), anyString(), anyString(), anyString());
+        // given
+        BDDMockito.given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
 
         when(mailSendHistoryRepository.save(any(MailSendHistory.class)))
                 .thenReturn(null);
